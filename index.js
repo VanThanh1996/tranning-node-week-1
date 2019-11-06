29e82c9c564e7cb1099cfbeeb8cbd0e21c906b14
VanPhuocGP <phamvanphuoc732@gmail.com>
2019-11-06 17:18:25 +0700

    Bai : 5,6,7,8,9,10

--- index.js
+++ index.js
@@ -65,10 +65,44 @@ app.post('/increment-item-in-array', function(req,res){
 
     res.send(Mang);
 });
+//bai5
+app.get('/:first_name/return-my-name', function (req,res){
+    console.log(req.params.first_name);
+    console.log(req.query.last_name);
+    res.sendStatus(200);
+});
+//bai6
+app.post('/turn-array',function (req,res){
+    var arr = req.body.arr;
+    // var mang = [];
+    // for (let i = arr.length - 1; i >= 0; i--) {
+    //     mang.push(arr[i])};
+    var mang = arr.reverse();
+    res.send({data: mang});
+});
 app.listen(3000, () => {
   console.log('app listen on port 3000 !');
 });
+//bai7
+app.get('/uppercase-my-name',function (req,res){
+    console.log(req.query.last_name.toUpperCase());
 
+    res.sendStatus(200);
+});
+//bai8
+app.get('/tolowercase-my-name',function (req,res){
+    console.log(req.query.last_name.toLowerCase());
 
+    res.sendStatus(200);
+});
+//bai9
+app.get('/:number/round-up-number',function(req,res){
+    console.log(Math.round(req.params.number));
+    res.sendStatus(200);
+});
+app.get('/:number/round-down-number',function(req,res){
+    console.log(Math.floor(req.params.number));
+    res.sendStatus(200);
+})
 // Văn Phước GP Code Here
 
